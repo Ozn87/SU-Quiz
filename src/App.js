7a5f90c9014c3fdcaf022028a27f56b69dc5f8df
Ozn87 <loera.osiris@gmail.com>
2022-05-08 19:54:43 -0600

    added data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import core1 from './data/core1.json'
 
-function App() {
+export default function App() {
+  let questions = []
+
+  for (let i in core1) {
+    questions.push(core1[i])
+  }
+
+  console.log(questions)
+
+  const [currentQuestion, setCurrentQuestion] = useState(99)
+  const [showScore, setShowScore] = useState(false)
+  const [score, setScore] = useState(0)
+
+  const handleAnswerOptionClick = (isCorrect) => {
+    if (isCorrect) {
+      setScore(score + 1)
+    }
+
+    const nextQuestion = currentQuestion + 1
+    if (nextQuestion < questions.length) {
+      setCurrentQuestion(nextQuestion)
+    } else {
+      setShowScore(true)
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {showScore ? (
+        <div className="score-section">
+          You scored {score} out of {questions.length}
+        </div>
+      ) : (
+        <>
+          <div className="question-section">
+            <div className="question-count">
+              <span>Question {currentQuestion + 1}</span>/{questions.length}
+            </div>
+            <div className="question-text">{questions[currentQuestion][1]}</div>
+          </div>
+          <div className="answer-section">
+            {questions[currentQuestion][2][0].map((answerOption) => (
+              <button
+                onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}
+              >
+                {answerOption}
+              </button>
+            ))}
+          </div>
+        </>
+      )}
     </div>
-  );
+  )
 }
-
-export default App;
